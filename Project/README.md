f1e032b22d036c720c34148a03b8984a7e477d9b
Nikhil God <nikhil0114@gmail.com>
2025-11-07 10:32:11 +0530

    added file

--- Project/README.md
+++
@@ -1,3 +0,0 @@
-Backend Project <br>
-eraser link :
--[model link]( 'https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj')
---
+++ connect_backend_frontend/erro.js
@@ -0,0 +1,7 @@
+const error = new Error("somnthing went wrong") ;
+console.log(error.message)
+console.log(error.name)
+// console.log(error.stack)
+console.log(error.statusCode = "400");
+console.log(error.success =true);
+console.log(error.status);
